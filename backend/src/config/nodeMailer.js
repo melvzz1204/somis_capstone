3d67333a9eaccce8eb257d9c2ac92245c642b43a
melvzz1204 <137977788+melvzz1204@users.noreply.github.com>
2026-08-22 14:07:46 +0800

    email production smtp

--- backend/src/config/nodeMailer.js
+++ backend/src/config/nodeMailer.js
@@ -11,6 +11,7 @@ const transporter = nodemailer.createTransport({
   host: emailHost,
   port: emailPort,
   secure: emailSecure,
+  family: 4,
   auth: {
     user: emailUser,
     pass: emailPassword,
